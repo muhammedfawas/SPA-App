ec2576e993a5cfd06adee16f1b3b5d97534fd2c3
fawas <fawzzhkz@gmail.com>
2020-01-06 13:59:28 +0530

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule}from '@angular/router'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PagenotFoundComponent } from './pagenot-found/pagenot-found.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagenotFoundComponent,
+    HeaderComponent,
+    ImageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot([
+  {path:'',component:HomeComponent},
+  {path:'Login',component:LoginComponent},
+  {path:'image',component:ImageComponent},
+  {path:'Register',component:RegisterComponent},
+  {path:'**',component:PagenotFoundComponent}
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
